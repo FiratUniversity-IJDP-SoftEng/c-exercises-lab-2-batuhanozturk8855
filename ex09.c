b1d96e1f840e38530d5319697da63d529b5a4866
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 13:58:04 +0300

    Implement count, sum, and average calculation
    Add a program to calculate the count, sum, and average of input numbers until -1 is entered.

--- ex09.c
+++ ex09.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    int count = 0;
+    int sum = 0;
+    double ort;
+
+    while (1) {
+        scanf("%d", &n);
+
+        if (n == -1)
+            break;
+
+        sum += n;
+        count++;
+    }
+
+    if (count > 0) {
+        ort = (double)sum / count;
+        printf("%d\n", count);
+        printf("%d\n", sum);
+        printf("%.2f\n", ort);
+    }
+
+    return 0;
+}
