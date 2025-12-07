410c3cbd5ed941856273e2a1e1aed6327d136777
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 13:15:42 +0300

    Correct variable names in printf for output
    Fix variable names in printf statement to match defined variables.

--- ex07.c
+++ ex07.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        int c1 = i * 5;
+        int c2 = 100 - ((i - 1) * 10 + 1);
+        int c3 = i;
+
+        printf("%d\t%d\t%d\n", col1, col2, col3);
+    }
+
+    return 0;
+}
