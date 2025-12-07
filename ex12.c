b8571a56f8e33c27dd2b6b50a060c5aab4a521b6
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 15:14:33 +0300

    Add factorial calculation in ex12.c
    Implement factorial calculation with input validation.

--- ex12.c
+++ ex12.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    long long result = 1;
+
+    do {
+        scanf("%d", &n);
+    } while (n < 0 || n > 20);
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    printf("%lld\n", result);
+
+    return 0;
+}
