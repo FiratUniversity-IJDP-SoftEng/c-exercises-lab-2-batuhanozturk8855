1817ce30a1d2ab98d6edb52d31e966089bc022c7
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 13:23:37 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[10];
+    int sum = 0;
+    double average;
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    average = (double)sum / 10;
+    printf("%d\n", sum);
+    printf("%.2f\n", average);
+
+    return 0;
+}
