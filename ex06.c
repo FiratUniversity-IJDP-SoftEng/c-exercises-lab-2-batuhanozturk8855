418269ea59291f76b042949abdde1386db1dbb66
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 13:13:30 +0300

    Implement number storage and printing in ex06.c
    Store numbers from 99 to 51 decrementing by 3 in an array and print them.

--- ex06.c
+++ ex06.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[20]; // fazladan tuttuk, önemli değil
+    int index = 0;
+    int i;
+
+    for (i = 99; i >= 51; i -= 3) {
+        numbers[index] = i;
+        index++;
+    }
+
+    // Yazdırma
+    for (i = 0; i < index; i++) {
+        printf("%d", numbers[i]);
+        if (i != index - 1) {
+            printf(", ");
+        }
+    }
+
+    return 0;
+}
