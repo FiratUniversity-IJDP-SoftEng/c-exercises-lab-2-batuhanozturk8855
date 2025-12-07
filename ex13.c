9312dab054c0d3895e8127c9030d4d0aad6154d9
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 15:30:30 +0300

    Implement factorial function and user input

--- ex13.c
+++ ex13.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return (long long)n * factorial(n - 1);
+}
+
+int main(void) {
+    int n;
+
+    do {
+        scanf("%d", &n);
+    } while (n < 0 || n > 20);
+
+    printf("%lld\n", factorial(n));
+
+    return 0;
+}
