eb8f3aca0391be1467c68956a88e502acaea7a76
batuhanozturk8855 <batuhanozturk8855@gmail.com>
2025-12-07 15:36:30 +0300

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+long long fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(void) {
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n; i++) {
+        printf("%lld ", fibonacci(i));
+    }
+    
+    printf("\n");
+
+    return 0;
+}
